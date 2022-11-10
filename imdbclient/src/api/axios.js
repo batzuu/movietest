aafd2b6ffb99c76da86c24b6e124dbdcc08ef2f0
batzuu <utkarsh1616@gmail.com>
2022-11-10 21:33:20 +0530

    FIXED ORDER

--- imdbclient/src/api/axios.js
+++ imdbclient/src/api/axios.js
@@ -3,14 +3,13 @@ let BASE_URL = "http://localhost:3500";
 const OMDB_API = "https://www.omdbapi.com";
 const OMDB_API_KEY = "a6c35cd0";
 
-export default axios.create({
-	baseURL: BASE_URL,
-});
-
 if (process.env.AB == "prod") {
 	BASE_URL =
 		"https://70a3-2405-201-4014-da9c-34ba-e5d8-88d2-ad35.in.ngrok.io/";
 }
+export default axios.create({
+	baseURL: BASE_URL,
+});
 
 export const axiosPrivate = axios.create({
 	baseURL: BASE_URL,
